d402b31910a968e0ee8a7d1c46ed14482d30c252
unknown <banhduchieucode@gmail.com>
2021-02-10 21:22:29 +0700

    first commit

---
+++ DBFirst/DBFirstDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBFirstDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new DBFirstDemoEntities();
+            var list = (from item in context.Posts select item);
+            foreach(var item in list)
+            {
+                Console.WriteLine(item.PostID + " : " + item.Title);
+            }
+            //Console.WriteLine(context.Posts.Find(1).Title);
+
+        }
+    }
+}
