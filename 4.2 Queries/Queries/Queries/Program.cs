dd6fee06885b8dd4a8e0b362a7a9384f5446977e
unknown <banhduchieucode@gmail.com>
2021-02-13 20:59:14 +0700

    Done Linq

--- 4.2 Queries/Queries/Queries/Program.cs
+++ 4.2 Queries/Queries/Queries/Program.cs
@@ -7,37 +7,41 @@ namespace Queries
     {
         static void Main(string[] args)
         {
+            LinqSyntax();
+        }
+        static void LinqSyntax() 
+        {
             var context = new PlutoContext();
 
             var query = from c in context.Courses
                         where c.Author.Id == 1
                         orderby c.Level descending, c.Name
                         select new { Name = c.Name, Author = c.Author.Name };
-            
+
             //GROUP
             var queryGroup = from c in context.Courses
-                             group c by c.Level 
+                             group c by c.Level
                              into g
                              select g;
-            foreach(var group in queryGroup)
+            foreach (var group in queryGroup)
             {
                 System.Console.WriteLine("{0} ({1})", group.Key, group.Count());
-                foreach(var course in group)
-                    System.Console.WriteLine("\t\t"+course.Name + " - level: " + course.Level);
+                foreach (var course in group)
+                    System.Console.WriteLine("\t\t" + course.Name + " - level: " + course.Level);
             }
 
             //JOIN
             var queryJoin = from c in context.Courses
                             join a in context.Authors on c.AuthorId equals a.Id
                             select new { CourseName = c.Name, AuthorName = a.Name };
-            foreach(var c in queryJoin)
+            foreach (var c in queryJoin)
                 System.Console.WriteLine("name: {0} - author: {1}", c.CourseName, c.AuthorName);
 
             //GROUP JOIN 
             var queryGrJoin = from a in context.Authors
                               join c in context.Courses on a.Id equals c.AuthorId into g
                               select new { AuthorName = a.Name, Course = g.Count() };
-            foreach(var x in queryGrJoin)
+            foreach (var x in queryGrJoin)
                 System.Console.WriteLine("{0} ({1})", x.AuthorName, x.Course);
 
             //CROSS JOIN
